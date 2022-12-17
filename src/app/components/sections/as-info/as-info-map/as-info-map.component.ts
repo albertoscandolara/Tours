a57ecd6cc63bcf72d0014f6ec8e828d2a1ab00e6
albertoscandolara <alberto.scandolara92.training@gmail.com>
2022-12-17 18:35:56 +0100

    Feat: #16 - Introduced leaflet map

--- src/app/components/sections/as-info/as-info-map/as-info-map.component.ts
+++ src/app/components/sections/as-info/as-info-map/as-info-map.component.ts
@@ -1,5 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { Tour } from 'src/app/models/tour';
+
+declare let L: {
+  map: (arg0: string) => {
+    (): any;
+    new (): any;
+    setView: { (arg0: number[], arg1: number): any; new (): any };
+  };
+  tileLayer: (
+    arg0: string,
+    arg1: { attribution: string }
+  ) => { (): any; new (): any; addTo: { (arg0: any): void; new (): any } };
+};
 
 @Component({
   selector: 'as-info-map',
@@ -11,14 +23,29 @@ import { Tour } from 'src/app/models/tour';
     './styles/as-info-map-watermelon.component.scss',
   ],
 })
-export class AsInfoMapComponent {
+export class AsInfoMapComponent implements AfterViewInit {
   /**
    * Current tour
    */
   @Input() Tour: Tour;
 
+  private _map: any = null;
+
   /**
    * Constructor
    */
   constructor() {}
+
+  ngAfterViewInit() {
+    this._initMap();
+  }
+
+  private _initMap(): void {
+    this._map = L.map('map').setView([51.505, -0.09], 13);
+    this._map.options.minZoom = 2;
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this._map);
+  }
 }
